3a61d5fa0b89caa8859c31551bfbad143636902d
Chuma747 <50207994+Chuma747@users.noreply.github.com>
2024-03-26 18:50:15 +0000

    copilot third commmit

--- comments.js
+++ comments.js
@@ -1,80 +1,83 @@
-//create web server application
-const express = require('express');
-const app = express();
-//create a port to listen to
-const port = 3000;
-//create a variable to store comments
-let comments = [];
-//create a variable to store the count of comments
-let count = 0;
-//create a function to add a comment
-function addComment(comment) {
-    comments.push(comment);
-    count++;
-}
-//create a function to get all comments
-function getComments() {
-    return comments;
-}
-//create a function to get all comments
-function getCommentsCount() {
-    return count;
-}
-//create a function to get all comments
-function clearComments() {
-    comments = [];
-    count = 0;
-}
-//create a function to get all comments
-function deleteComment(index) {
-    comments.splice(index, 1);
-    count--;
-}
-//create a function to get all comments
-function updateComment(index, comment) {
-    comments[index] = comment;
-}
-//create a function to get all comments
-function getComment(index) {
-    return comments[index];
-}
-//create a function to get all comments
-function getCommentIndex(index) {
-    return comments[index];
-}
-//create a function to get all comments
-function getCommentIndex(index) {
-    return comments[index];
-}
-//create a function to get all comments
-app.get('/comments', (req, res) => {
-    res.send(getComments());
-});
-//create a function to get all comments
-app.get('/comments/count', (req, res) => {
-    res.send(getCommentsCount());
-});
-//create a function to get all comments
-app.post('/comments', (req, res) => {
-    addComment(req.query.comment);
-    res.send(getComments());
-});
-//create a function to get all comments
-app.post('/comments/clear', (req, res) => {
-    clearComments();
-    res.send(getComments());
-});
-//create a function to get all comments
-app.delete('/comments/:index', (req, res) => {
-    deleteComment(req.params.index);
-    res.send(getComments());
-});
-//create a function to get all comments
-app.put('/comments/:index', (req, res) => {
-    updateComment(req.params.index, req.query.comment);
-    res.send(getComments());
-});
-//create a function to get all comments
-app.get('/comments/:index', (req, res) => {
-    res.send(getComment(req.params.index));
+// Create web server
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+var mime = require('mime');
+var cache = {};
 
+function send404(response) {
+    response.writeHead(404, {'Content-Type': 'text/plain'});
+    response.write('Error 404: resource not found.');
+    response.end();
+}
+
+function sendFile(response, filePath, fileContents) {
+    response.writeHead(200, {'Content-Type': mime.lookup(path.basename(filePath))});
+    response.end(fileContents);
+}
+
+function serveStatic(response, cache, absPath) {
+    if (cache[absPath]) {
+        sendFile(response, absPath, cache[absPath]);
+    } else {
+        fs.exists(absPath, function(exists) {
+            if (exists) {
+                fs.readFile(absPath, function(err, data) {
+                    if (err) {
+                        send404(response);
+                    } else {
+                        cache[absPath] = data;
+                        sendFile(response, absPath, data);
+                    }
+                });
+            } else {
+                send404(response);
+            }
+        });
+    }
+}
+
+var server = http.createServer(function(request, response) {
+    var filePath = false;
+
+    if (request.url == '/') {
+        filePath = 'public/index.html';
+    } else {
+        filePath = 'public' + request.url;
+    }
+
+    var absPath = './' + filePath;
+    serveStatic(response, cache, absPath);
+});
+
+server.listen(3000, function() {
+    console.log("Server listening on port 3000.");
+});
+
+var chatServer = require('./lib/chat_server');
+chatServer.listen(server);
+// End
+// Path: chat_server.js
+// Create chat server
+var socketio = require('socket.io');
+var io;
+var guestNumber = 1;
+var nickNames = {};
+var namesUsed = [];
+var currentRoom = {};
+
+exports.listen = function(server) {
+    io = socketio.listen(server);
+    io.set('log level', 1);
+    io.sockets.on('connection', function(socket) {
+        guestNumber = assignGuestName(socket, guestNumber, nickNames, namesUsed);
+        joinRoom(socket, 'Lobby');
+        handleMessageBroadcasting(socket, nickNames);
+        handleNameChangeAttempts(socket, nickNames, namesUsed);
+        handleRoomJoining(socket);
+        socket.on('rooms', function() {
+            socket.emit('rooms', io.sockets.manager.rooms);
+        });
+        // Add the closing brace here
+    });
+};
